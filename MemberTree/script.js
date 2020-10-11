a97e5f584a9d772bc91a088c4214d1153821b802
zzHee <772575675@qq.com>
2020-10-11 22:22:06 +0800

    delete the jq.js

--- MemberTree/script.js
+++ MemberTree/script.js
@@ -144,9 +144,6 @@ var baseText = null;
                     .on("click", click);
 
                 //添加连接点---此处设置的是圆圈过渡时候的效果（颜色）
-                // nodeEnter.append("circle")
-                //   .attr("r", 1e-6)
-                //   .style("fill", function(d) { return d._children ? "lightrgba(0, 136, 0,0.7)" : "rgba(0, 136, 0,0.7)"; });//d 代表数据，也就是与某元素绑定的数据。
                 nodeEnter.append("rect")
                     .attr("x", -35)
                     .attr("y", -22)
@@ -160,7 +157,6 @@ var baseText = null;
                         return d.children || d._children ? 13 : 13;
                     })
                     .attr("dy", "-4")
-                    // .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
                     .attr("text-anchor", "middle")
                     .text(function(d) {
                         return d.name;
@@ -168,24 +164,6 @@ var baseText = null;
                     .style("fill", "#289dde")
                     .style("fill-opacity", 1e-6);
 
-                /*nodeEnter.append("line")
-                    .attr("x1", "-25")
-                    .attr("y1", "0")
-                    .attr("x2", "50")
-                    .attr("y", "0")
-                    .attr("stroke", "white")
-
-                nodeEnter.append("text")
-                    .attr("x", function(d) {
-                        return d.children || d._children ? 13 : 13;
-                    })
-                    .attr("dy", "10")
-                    .attr("text-anchor", "middle")
-                    .text(function(d) {
-                        return d.name;
-                    })
-                    .style("fill", "white")
-                    .style("fill-opacity", 1);*/
                 // Transition nodes to their new position.将节点过渡到一个新的位置-----主要是针对节点过渡过程中的过渡效果
                 //node就是保留的数据集，为原来数据的图形添加过渡动画。首先是整个组的位置
                 var nodeUpdate = node.transition() //开始一个动画过渡
@@ -216,8 +194,6 @@ var baseText = null;
                     })
                     .remove();
 
-                // nodeExit.select("circle")
-                //   .attr("r", 1e-6);
                 nodeExit.select("rect")
                     .attr("x", -35)
                     .attr("y", -22)
@@ -227,11 +203,8 @@ var baseText = null;
                     .style("fill", "#eff4fa");
 
                 nodeExit.select("text")
-                    // .attr("text-anchor", "middle")
                     .style("margin-top", "10");
-                    // .style("fill-opacity", 1e-6);
 
-                // Update the links…线操作相关
                 //再处理连线集合
                 var link = svg.selectAll("path.link")
                     .data(links, function(d) {
@@ -295,8 +268,7 @@ var baseText = null;
         }
         /*-----------------------初始化函数结束*------------------------*/
 
-        // function check(nodes, find_name, may_need);
-        // function add_tree(node, innerTree);
+
         //自定义json长度查找函数,返回json树的下层子节点的长度(个数)
         function getJsonLength(jsonData) {
             var jsonLength = 0;
@@ -313,69 +285,33 @@ var baseText = null;
         //@nodes 源json树
         //@find_name 要找的导师名
         //@may_need 可能需要添加的json树 
-        function check(nodes, find_name, may_need, keke) {
-            var fanhui1 = 0;
-            var fanhui2 = 1;
-            // alert("checkcheckcheckcheckcheckcheck");
+        function Reconstruct(nodes, find_name, may_need, keke) {
             var length_now = getJsonLength(nodes.children);
-            // alert("chang = " + length_now)
             for (var ll = 0; ll < length_now; ll++) {
-                // alert(nodes.children[ll].name);
-                if (nodes.children[ll].name == find_name) { //第ll个子节点的名字是否与要查找的相同
-                    // alert("hhhhhhhhhhhhhh");
+                //查找此节点名是否与传入名相同
+                if (nodes.children[ll].name == find_name) {
                     quanju_flag = 1;
-                    //add_tree(nodes.children[ll].children, may_need, keke);
-                    // alert("add");
-                    // alert(getJsonLength(may_need));
-                    nodes.children[ll] = may_need; //将该json树添加到儿子节点作为关联
-                    // alert("add success");
-                    return fanhui2;
+                    nodes.children[ll] = may_need; //将该传入树添加到儿子节点作为关联
+                    return 1;
                 } else {
-                    check(nodes.children[ll], find_name, may_need, keke);
-                    // return;
+                    Reconstruct(nodes.children[ll], find_name, may_need, keke);
                 }
             }
-
-            return fanhui1;
+            return 0;
         }
 
-        // function add_tree(node, innerTree, keke) {
-        //     alert("add");
-        //     node = innerTree;
-        //     //treeData[]
-        //     shuInit(keke);
-        //     alert("add success");
-        // }
-        // //@ node 节点添加
-        // //@ innerTree 添加的内容
-        // function add_tree(node, innerTree) {
-        //     alert("add");
-        //     node.children = innerTree;
-        // }
-
-        //追逐函数
+        //函数
         /*
         分割传输过来的数据并构造json树结构
         相当于主函数功能
         */
         function chase() {
-            //treeData.join("node");
-            //alert(treeData[0].name);
-            //treeData = [{}];
-            // alert("Hello!!");
-            // alert(treeData.name);
-            // treeData.name = "Test";
-            //update(source);
-            // treeData[0].children[0].name = "sbbbbb";
-            //console.log(treeData);
             var count = 0; //定义儿子节点的编号
             var flag = 0; //定义标志是否为关联树值为1
             // var flag = 0;
             // var fflag = 0;
             // var daoshi = [];
             var all_data = document.getElementById("user").value;
-            console.log(all_data);
-            //alert(xx);
             var sclice_data = [];
             var model_data = [];
 
@@ -388,32 +324,24 @@ var baseText = null;
                 count = 0;
                 //flag = 0;
                 quanju_flag = 0;
-                count_shu = 0
-                    //treeData[j] = [{}];
-                    //shuInit();
+                count_shu = 0;
                 sclice_data = model_data[j].split("\n");
 
                 for (var i = 0; i < sclice_data.length; ++i) {
                     var head_tmp = "";
                     var body_tmp = "";
-                    //fflag = 0;
 
                     var hb = sclice_data[i].split("："); //从冒号分割一层字符串
                     head_tmp = hb[0];
                     body_tmp = hb[1];
                     //处理冒号前的部分
                     if (head_tmp == "导师") {
-                        //document.write("<br /><br />"+body_tmp+"<br />");
                         var daoshi2 = {
                             "name": body_tmp,
                             "parent": "null",
                             "children": [{}]
                         }
                         treeData[j] = daoshi2; //将导师嵌入节点
-                        // console.log(treeData);
-                        //update(source);
-                        //弹出导师名
-                        //alert(treeData[j].name);
 
                     } else if(/[1-9][0-9]*/.test(head_tmp) == false) {
                        var children = {
@@ -432,63 +360,33 @@ var baseText = null;
                        }
                        count++; //第二子节点编号加一，生成下一个第二子节点
                     } else {
-
-                        //document.write("<br />");
                         var children = {
                             "name": head_tmp,
                             "parent": "null",
                             "children": [{}]
                         }
                         treeData[j].children[count] = children; //将年级及职业嵌入节点
-                        //弹出年纪介绍
-                        //alert(treeData[j].children[count].name);
-                        //分割2016级博士生
-                        // var heads = head_tmp.split("级");
 
-                        // treeData[j].children[count].name = heads[0] + "级";
-                        // console.log(treeData);
-                        // treeData[j].children[count].children[count].name = heads[1];
-                        // console.log(treeData);
-                        //document.write("年级："+heads[0]+"<br />");
-                        //document.write("职业："+heads[1]+"<br />");
+
                         //处理冒号后的部分
                         var bodies = body_tmp.split("、");
-                        //document.write("姓名：");
                         for (var kk = 0; kk < bodies.length; ++kk) {
-                            //document.write("|");
-                            //document.write(bodies[kk]+"|");	
                             var children = {
                                     "name": bodies[kk],
                                     "parent": "null",
-                                    //"children": [{}]
                                 }
-                                //treeData.push(children);
                             treeData[j].children[count].children[kk] = children; //将姓名嵌入节点
-                            //console.log(treeData);
-                            //弹出学生名
-                            //alert(treeData[j].children[count].children[kk].name);
-
 
                         }
                         count++; //第二子节点编号加一，生成下一个第二子节点
                     }
                 }
                 //一棵树型数据构造完成
-                //alert(treeData[j].length);
                 //和前面所有的树比较，判断是否为关联树
                 var tree_tmp = treeData[j];
                 var name_tmp = treeData[j].name;
                 for (num_tmp = 0; num_tmp < j; num_tmp++) {
-                    //alert("num: " + num_tmp);
-                    // alert("length = " + getJsonLength(treeData[num_tmp].children[0].children));
-                    // alert(treeData[num_tmp]);
-                    // alert(treeData[j].name);
-                    // alert(treeData[j]);
-                    check(treeData[num_tmp], name_tmp, tree_tmp, num_tmp);
-                    //alert("flag = " + flag);
-
-                    // alert(check(treeData[num_tmp], treeData[j].name, treeData[j]));
-                    // alert(test("hhhhhhhhhhhhh"));
+                    Reconstruct(treeData[num_tmp], name_tmp, tree_tmp, num_tmp);
                 }
                 if (!quanju_flag) count_shu++; //若有关联树则独立树的棵数不增加
 
@@ -498,7 +396,6 @@ var baseText = null;
             }
 
             //生成所有树
-            // alert("shu: " + count_shu);
             for (var i = 0; i <= count_shu; i++) {
                 shuInit(i)
             }
@@ -507,20 +404,6 @@ var baseText = null;
         //@nodes 源json树
         //@find_name 要找的导师名
         //@may_need 可能需要添加的json树       
-        // function check(nodes, find_name, may_need) {
-        //     for (var ll = 0; ll < nodes.length; ll++) {
-        //         if (nodes.children[i] == "null") {
-        //             if (nodes.name == find_name) {
-        //                 add_tree(nodes, may_need);
-        //                 return;
-        //             }
-        //         } else {
-        //             check(nodes.children[ll], find_name, may_need);
-        //             return;
-        //         }
-
-        //     }
-        // }
         // //@ node 节点添加
         // //@ innerTree 添加的内容
         // function add_tree(node, innerTree) {
